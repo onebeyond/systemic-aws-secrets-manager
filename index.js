0ae95a2b405d48dbb1c95cad607c810dd69572b3
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2022-02-14 17:07:19 +0100

    refactor: remove unused commandExecutor

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 const debug = require('debug')('systemic-aws-s3');
-const { SecretsManager, SecretsManagerClient } = require('@aws-sdk/client-secrets-manager');
-const path = require('path');
-
-const commands = require('require-all')(path.resolve(__dirname, '/commands'));
+const { SecretsManagerClient } = require('@aws-sdk/client-secrets-manager');
 
 let client = null;
-let aggregatedSecretsManager = null;
 
 module.exports = () => {
   const start = async ({ config }) => {
     debug('Initializing SecretsManagerClient');
-    aggregatedSecretsManager = new SecretsManager(config);
     client = new SecretsManagerClient(config);
 
     return {
-      client,
-      commandExecutor: commands.commandExecutor(aggregatedSecretsManager)
+      client
     };
   };
 
